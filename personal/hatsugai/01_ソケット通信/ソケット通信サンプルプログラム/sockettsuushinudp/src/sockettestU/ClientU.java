2116b1a009d14dcc37936ee865d831f758eb57bd
ptechatsugai <hatsugai@p-tec.jp>
2017-06-26 17:50:56 +0900

    UDPの送信用サンプルプログラムを改良しました。

--- personal/hatsugai/01_ソケット通信/ソケット通信サンプルプログラム/sockettsuushinudp/src/sockettestU/ClientU.java
+++ personal/hatsugai/01_ソケット通信/ソケット通信サンプルプログラム/sockettsuushinudp/src/sockettestU/ClientU.java
@@ -8,17 +8,23 @@ public class ClientU {
 
 	public static void main(String[] args) {
 		try {
-            InetAddress addr = InetAddress.getByName(args[0]);//サーバーのあるIPアドレス
+            System.out.println("conf run");
+			InetAddress addr = InetAddress.getByName(args[0]);//サーバーのあるIPアドレス
             int port = Integer.parseInt(args[1]); //サーバ側の待受ポート番号
 
-            // 送信するパケットを生成
-            byte buffer[] = args[2].getBytes();
-            DatagramPacket dp = new DatagramPacket(buffer, buffer.length, addr, port);
+            // 送信するパケットを生成(一定数ループさせる）
+            for(int i =0;i<=2000000;i++){
+            	byte buffer[] = (i+args[2]).getBytes();//送信するメッセージとか番号とか
+            	DatagramPacket dp = new DatagramPacket(buffer, buffer.length, addr, port);
 
-            // ソケットを生成してパケットを送信
-            DatagramSocket ds = new DatagramSocket();
-            ds.send(dp);
-            ds.close();
+            	// ソケットを生成してパケットを送信
+            	DatagramSocket ds = new DatagramSocket();
+
+            	ds.send(dp);
+
+            	ds.close();
+            }
+
         }
         catch (Exception e) {
             e.printStackTrace();
